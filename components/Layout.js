7839367317fd2c9eb6d9875b708ca12eb75e4cd2
Jniouen-Wassim <jniouenwassim@gmail.com>
2021-04-20 13:11:31 +0200

    layout footer

---
+++ components/Layout.js
@@ -0,0 +1,37 @@
+export default function Layout() {
+  return (
+    <>
+      <footer>
+        <div class="container">
+          <div class="grid-container">
+            <div class="ABOUT">
+              <h3>ABOUT</h3>
+              <p>Integer posuere erat a ante venenati dapibus posuere velit aliquet. 
+                  Fusce dapibus, tellus cursus commodo, tortor mauris sed posuere.</p>
+            </div>
+            <div class="our-service">
+              <h3 class="titreCopyright">OUR SERVICE</h3>
+              <p>Graphic Design</p>
+              <p>Web Design</p>
+              <p>Web Development</p>
+            </div>
+            <div class="quick-links">
+              <h3 class="titreCopyright">QUICK LINKS</h3>
+            </div>
+            <div class="subscribe-now">
+              <h3 class="titreCopyright">SUBSCRIBE NOW</h3>
+            </div>
+          </div>
+
+          <div class="copyright">
+            <p class="pCopyright" >
+              COPYRIGHT 2018. ALL RIGHTS RESERVED. <br/> 
+              Design And Developed By Themefisher.Com <br/> 
+              Get More <span class="spanCopyright"> Free Bootstrap Templates </span> 
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
