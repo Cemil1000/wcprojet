249a95457b5820874633624a3192afc483d7ef93
cemil <cemilyilmaz.be@gmail.com>
2021-04-21 14:20:49 +0200

    section 2 test

---
+++ components/Section2.js
@@ -0,0 +1,11 @@
+export default function Section2() {
+    return (
+        <>
+            <section id="section2">
+                <div className="container">
+
+                </div>
+            </section>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Layout from "../components/Layout"
 import Section1 from "../components/Section1"
+import Section2 from "../components/Section2"
 
 export default function Home() {
   return (
     <>
-      <Section1 />
+      <Layout>
+        <Section2 />
+      </Layout>
     </>
   )
 }
