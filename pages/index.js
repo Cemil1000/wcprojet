f4ed87af6ef6a5bdaa29eceb365f501854920c8d
Jniouen-Wassim <jniouenwassim@gmail.com>
2021-04-21 16:34:55 +0200

    allAbout

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout from "../components/Layout"
 import Section1 from "../components/Section1"
 import HomeVideo from "../components/HomeVideo"
+import Section2 from "../components/Section2"
+
 
 export default function Home() {
   return (
@@ -9,6 +11,7 @@ export default function Home() {
       <Layout>
         <HomeVideo />
         <Section1 />
+        <Section2 />
       </Layout>
     </>
   )
