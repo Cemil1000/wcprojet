004702356db91f8f80015af07adf948f1482c2b3
Jniouen-Wassim <jniouenwassim@gmail.com>
2021-04-22 14:18:20 +0200

    all section 3

---
+++ components/Section3.js
@@ -0,0 +1,71 @@
+import { VscBriefcase } from "react-icons/Vsc";
+import { IoAlarmOutline } from "react-icons/Io5";
+import { AiOutlineMail, AiFillLock } from "react-icons/ai";
+
+const allCards = [
+  {
+    count: "1.",
+    icon: <VscBriefcase size={"40"} className="icon-Section3" />,
+    title: "Project Analysis",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus. Nullam iaculis arcu at mauris dapibus consectetur.",
+  },
+
+  {
+    count: "2.",
+    icon: <IoAlarmOutline size={"40"} className="icon-Section3" />,
+    title: "Time Management",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus. Nullam iaculis arcu at mauris dapibus consectetur.",
+  },
+
+  {
+    count: "3.",
+    icon: <AiOutlineMail size={"40"} className="icon-Section3" />,
+    title: "Mail Support",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus. Nullam iaculis arcu at mauris dapibus consectetur.",
+  },
+
+  {
+    count: "4.",
+    icon: <AiFillLock size={"40"} className="icon-Section3" />,
+    title: "Secure System",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur.. Sed id lorem eget orci dictum facilisis vel id tellus. Nullam iaculis arcu at mauris dapibus consectetur.",
+  },
+];
+
+const cardsList = allCards.map((elem) => {
+  return (
+    <>
+      <div class={"card "}>
+        <span className={"count"}>{elem.count}</span>
+        {elem.icon}
+        <h4>{elem.title}</h4>
+        <p>{elem.description}</p>
+      </div>
+    </>
+  );
+});
+
+export default function Section3() {
+  return (
+    <>
+      <section id="section3">
+        <div className="container">
+          <div id="titre-general">
+            <h4>HOW WE WORKS</h4>
+            <h2>Our Work Process</h2>
+            <span></span>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
+              reiciendis quasi itaque, obcaecati atque sit!
+            </p>
+          </div>
+          <div class="cardsOurWork">{cardsList}</div>
+        </div>
+      </section>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Layout from "../components/Layout"
 import Section1 from "../components/Section1"
 import Section2 from "../components/Section2"
+import Section3 from "../components/Section3"
 import HomeVideo from "../components/HomeVideo"
 
 
@@ -12,6 +13,7 @@ export default function Home() {
         <HomeVideo />
         <Section1 />
         <Section2 />
+        <Section3 />
       </Layout>    
     </>
   )
