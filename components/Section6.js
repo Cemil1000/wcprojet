e5e7f306a1e133f16e0fc296950ff7aca8445324
cemil <cemilyilmaz.be@gmail.com>
2021-04-23 11:55:15 +0200

    correction code

--- components/Section6.js
+++ components/Section6.js
@@ -1,8 +1,4 @@
-import { ImFacebook, ImDribbble} from "react-icons/im";
-import { IoLogoTwitter } from "react-icons/Io";
-import { TiSocialGooglePlus } from "react-icons/ti";
-
-export default function Section5() {
+export default function Section6() {
     const team =[
         {
             img: "img/team/member-1.jpg",
@@ -33,17 +29,14 @@ export default function Section5() {
     const personne = team.map(elem=>{
         return(
             <>
-                <div className="card">
+                <div className="card ">
                     <img src={elem.img} alt="Portrait photo"/>
                     <div className="card-body">
                         <h3>{elem.nom}</h3>
                         <span>{elem.role}</span>
                         <p>{elem.descprition}</p>
                         <ul>
-                            <li><a href=""><ImFacebook size={"18"} className="logo"/></a></li>
-                            <li><a href=""><IoLogoTwitter size={"18"} className="logo"/></a></li>
-                            <li><a href=""><TiSocialGooglePlus size={"23"} className="logo"/></a></li>
-                            <li><a href=""><ImDribbble size={"18"} className="logo"/></a></li>
+                            
                         </ul>
                     </div>
                 </div>
@@ -55,13 +48,28 @@ export default function Section5() {
         <>
             <section id="section5">
                 <div id="titre-general">
-                    <h4>Team member</h4>
-                    <h2>Our Team.</h2>
+                    <h4>Easy pricing</h4>
+                    <h2>Princing.</h2>
                     <span></span>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum reiciendis quasi itaque, obcaecati atque sit!</p>
                 </div>
                 <div className="container cards-Section5">
-                    {personne}
+                    <div className="card">
+                        <div>
+                            <img src="./img/about/about-1.jpg" alt="Portrait photo"/>
+                        </div>
+                        
+                        <div className="card-body">
+                            <ul>
+                                <li>1GB Disk Space</li>
+                                <li>10 Email Account</li>
+                                <li>Script Installer</li>
+                                <li>1 GB Storage</li>
+                                <li>10 GB Bandwidth</li>
+                                <li>24/7 Tech Support</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
