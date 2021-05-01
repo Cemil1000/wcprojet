8cfd4743ecd9cb5003573a92d12ddab4e798c1d2
Jniouen-Wassim <jniouenwassim@gmail.com>
2021-05-01 16:54:12 +0200

    commit navBar

--- components/Layout.js
+++ components/Layout.js
@@ -1,70 +1,92 @@
-import {FaCaretDown}from 'react-icons/fa'
-
+import { FaCaretDown } from "react-icons/fa";
+import { AiOutlineMenu } from "react-icons/ai";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 export default function Layout({ children }) {
-    return (
-        <>
-            <header>
-                <div className="container">
-                    <img src="./img/logo.png" alt="Logo du site" className="logo"/>
-                    <nav>
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Service</a></li>
-                            <li><a href="#">Portfolio</a></li>
-                            <li><a href="#">Contact</a></li>
-                            <li className="dropdown">
-                                <a href="#" className="dropbtn">Pages <FaCaretDown /></a>
-                                <div className="dropdown-content">
-                                    <a href="#">404 Page</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
+  let iconStyles = { color: "white", fontSize: "1.5em" };
+  
+  return (
+    <>
+      <header>
+        <div className="container">
+          <Navbar bg="" expand="lg">{/* expand="lg" <=Burger Menu */}
 
-            {children}
+            <Navbar.Brand href="#home">
+              <img src="./img/logo.png" className="logo d-inline-block align-top"alt="Logo du site"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"> <AiOutlineMenu style={iconStyles} /> </Navbar.Toggle>{/* <=Burger Menu */}
+            <Navbar.Collapse id="basic-navbar-nav"className="justify-content-end">
+              <Nav.Link id="home" href="#">Home</Nav.Link>
+              <Nav.Link href="#">About</Nav.Link>
+              <Nav.Link href="#">Service</Nav.Link>
+              <Nav.Link href="#">Portfolio</Nav.Link>
+              <Nav.Link href="#">Contact</Nav.Link>
+              <NavDropdown title="Pages" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">404</NavDropdown.Item>
+              </NavDropdown>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      </header>
 
-            <footer>
-                    <div className="container">
-                        <div>
-                            <h3>ABOUT</h3>
-                            <p>Integer posuere erat a ante venenati dapibus posuere velit aliquet. 
-                            Fusce dapibus, tellus cursus commodo, tortor mauris sed posuere.</p>
-                        </div>
+      {children}
 
-                        <div>
-                            <h3>OUR SERVICE</h3>
-                            <a href="#">Graphic Design</a>
-                            <a href="#">Web Design</a>
-                            <a href="#">Web Development</a>
-                        </div>
+      <footer>
+        <div className="container">
+          <div>
+            <h3>ABOUT</h3>
+            <p>
+              Integer posuere erat a ante venenati dapibus posuere velit
+              aliquet. Fusce dapibus, tellus cursus commodo, tortor mauris sed
+              posuere.
+            </p>
+          </div>
 
-                        <div>
-                            <h3>QUICK LINKS</h3>
-                            <a href="#">Partners</a>
-                            <a href="#">About</a>
-                            <a href="#">FAQ’s</a>
-                            <a href="#">Badges</a>
-                        </div>
+          <div>
+            <h3>OUR SERVICE</h3>
+            <a href="#">Graphic Design</a>
+            <a href="#">Web Design</a>
+            <a href="#">Web Development</a>
+          </div>
 
-                        <div>
-                            <h3>SUBSCRIBE NOW</h3>
-                            <input type="email" className="inputEmail" aria-describedby="emailHelp" placeholder="Enter email"></input>
-                            <button type="submit" className="btnFooter ">Subscribe Now</button>
-                        </div>
-                    </div>
+          <div>
+            <h3>QUICK LINKS</h3>
+            <a href="#">Partners</a>
+            <a href="#">About</a>
+            <a href="#">FAQ’s</a>
+            <a href="#">Badges</a>
+          </div>
 
-                    <div className="copyright">
-                        <p className="pCopyright" >
-                        COPYRIGHT 2018. ALL RIGHTS RESERVED. <br/> 
-                        Design And Developed By Themefisher.Com <br/> 
-                        Get More <span className="spanCopyright"> Free Bootstrap Templates </span> 
-                        </p>
-                    </div>
-            </footer>
-        </>
-    )
+          <div>
+            <h3>SUBSCRIBE NOW</h3>
+            <input
+              type="email"
+              className="inputEmail"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            ></input>
+            <button type="submit" className="btnFooter ">
+              Subscribe Now
+            </button>
+          </div>
+        </div>
+
+        <div className="copyright">
+          <p className="pCopyright">
+            COPYRIGHT 2018. ALL RIGHTS RESERVED. <br />
+            Design And Developed By Themefisher.Com <br />
+            Get More
+            <span className="spanCopyright"> Free Bootstrap Templates </span>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 }
